63cbb164fe1f9534ca50fec276779e29664f467f
无名 <luoyonghua@cai-inc.com>
2018-06-29 09:41:43 +0800

    modify

--- src/main/java/io/mycat/util/SelectorUtil.java
+++ src/main/java/io/mycat/util/SelectorUtil.java
@@ -20,6 +20,12 @@ public class SelectorUtil {
 
     public static final long MIN_SELECT_TIME_IN_NANO_SECONDS = 500000L;
 
+    /**
+     * 模仿netty, 解决epoll空转的bug
+     * @param oldSelector
+     * @return
+     * @throws IOException
+     */
     public static Selector rebuildSelector(final Selector oldSelector) throws IOException {
         final Selector newSelector;
         try {
@@ -35,7 +41,14 @@ public class SelectorUtil {
                 for (SelectionKey key: oldSelector.keys()) {
                     Object a = key.attachment();
                     try {
-                        // invalid, or registered with the newSelector
+                        /**
+                         *  invalid, or registered with the newSelector
+                         *  keyFor: 返回与该通道和指定的选择器相关的键
+                         *
+                         *  <p>当通道关闭时，所有相关的键会自动取消（记住，一个通道可以被注册到多个选择器上）。当
+                         * 选择器关闭时，所有被注册到该选择器的通道都将被注销，并且相关的键将立即被无效化（取
+                         * 消）。一旦键被无效化，调用它的与选择相关的方法就将抛出 CancelledKeyException</p>
+                          */
                         if (!key.isValid() || key.channel().keyFor(newSelector) != null) {
                             continue;
                         }
